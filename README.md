fe2c9bd03d85d8c0aaea4134bb592bfbc4531fc4
Mark Lassoff <mark@learntoprogram.tv>
2017-05-10 16:06:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#terminology for Learning Solutions Magazine
---
+++ terminology.js
@@ -0,0 +1,22 @@
+var vocab = [
+    "IDE",
+    "Version Control",
+    "Standards and Conventions",
+    "Libraries",
+    "Frameworks",
+    "Tools",
+    "Languages",
+    "Sprites",
+    "JSON" ];
+
+var definitions = [
+    "IDE or Integrated Development Environment is a software application that provide an integrated set of tools for software development. There are several IDE's that specialize in a particular language or technology. For example X-Code allows programmers to use the languages Swift and Objective C to build applications that run specifically on Apple products.",
+    "Software that allows you to record changes to a set of code and enables recall to an earlier version of your code base. Git is a commonly used version control software.",
+    "Guidelines for specific programming languages and technologies that recommend best practices for code style, documentation, testing, software architecture and naming conventions.",
+    "A collection of resources used to build applications. Libraries may include prewritten code, precomplied code or templates that are used in the development process, often doing “heavy lifting” within a particular development technology.",
+    "Frameworks often provide a generic high-level implementation for software in certain problem areas. The Frameworks are meant to be modified to create specific solutions within a problem space.",
+    "In coding vocabulary, tools are used to make certain parts of the software development process easier for the programmer. A debugger is a tool used to aid in finding bugs within software.",
+    "Programming languages provide a particular vocabulary and set of syntax for developing applications. Common programming languages include Javascript, C, and Ruby.",
+    "An individual image element that will appear in an application. Most often associated with game programming a sprite might represent a single state in an animation of a character walking.",
+    "JSON is an acronym for Javascript Object Notation. JSON is a platform independent, all text, platform neutral way of exchanging data between systems. Because JSON notation is generic, it is free from a particular system implementation. It is used commonly with many different platforms."
+];
